8b38f1b8e91925abea89693620eb66f6acb41fa2
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-10 16:29:27 -0300

    Update Button.tsx

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,17 +11,19 @@ interface ButtonProps {
   fullWidth?: boolean;
   icon?: React.ReactNode;
   className?: string;
+  style?: React.CSSProperties;
 }
 
 export function Button({
   children, variant = 'primary', size = 'md', onClick,
   type = 'button', disabled = false, fullWidth = false, icon,
-  className = ''
+  className = '', style
 }: ButtonProps) {
   return (
     <button
       className={`btn btn-${variant} btn-${size} ${fullWidth ? 'btn-full' : ''} ${className}`}
       onClick={onClick} type={type} disabled={disabled}
+      style={style}
     >
       {icon && <span className="btn-icon">{icon}</span>}
       <span className="btn-text">{children}</span>
