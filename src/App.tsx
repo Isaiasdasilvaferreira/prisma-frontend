d7b763c4d8875e06bb11f9085f30a7cf67d295ef
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-15 18:25:03 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import { AppRoutes } from './routes/AppRoutes';
+import { Analytics } from '@vercel/analytics/react';
 import './styles/globals.css';
 
 function App() {
   return (
     <AuthProvider>
       <AppRoutes />
+      <Analytics />
     </AuthProvider>
   );
 }
