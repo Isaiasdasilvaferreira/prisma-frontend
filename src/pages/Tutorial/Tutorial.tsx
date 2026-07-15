c459598de48f8e0aee908e00096106490049975d
Isaias da Silva Ferreira <isaiasdasilvaf52@gmail.com>
2026-07-15 19:28:43 -0300

    Update Tutorial.tsx

--- src/pages/Tutorial/Tutorial.tsx
+++ src/pages/Tutorial/Tutorial.tsx
@@ -77,7 +77,7 @@ export function Tutorial() {
                   <iframe
                     width="100%"
                     height="100%"
-                    src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+                    src=""
                     title="Prisma Tutorial"
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
